cbd838b8e3fffa559290040b5514df3bdf91384e
Upendra Prabhakar Bhat (Europe - iDEAS-AEM) <up40063884@wipro.com>
2021-04-04 10:23:08 +0530

    fourth commit

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/", function(req, res) {
         if (response.responsecode === 200) {
             res.sendFile(__dirname + "/success.html")
         } else {
-            res.sendFile(__dirname + "failure.html")
+            res.sendFile(__dirname + "/failure.html")
         }
 
         response.on("data", function(data) {
